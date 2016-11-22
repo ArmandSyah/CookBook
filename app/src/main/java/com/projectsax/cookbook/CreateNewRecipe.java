7c9322e0e0aa0cbccda96cd0413a766faee34ab6
ArmandSyah <Armand.Syahtama@gmail.com>
2016-11-21 20:14:01 -0500

    Finished Deliverable 3

--- app/src/main/java/com/projectsax/cookbook/CreateNewRecipe.java
+++ app/src/main/java/com/projectsax/cookbook/CreateNewRecipe.java
@@ -1,7 +1,10 @@
 package com.projectsax.cookbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateNewRecipe extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class CreateNewRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_recipe);
+
+        Button createIngredientBtn = (Button) findViewById(R.id.make_ingredient_btn);
+        Button createInstrucitonBtn = (Button) findViewById(R.id.make_instruction_btn);
+
+        createIngredientBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateNewRecipe.this, NewIngredient.class));
+            }
+        });
+
+        createInstrucitonBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateNewRecipe.this, NewInstruction.class));
+            }
+        });
     }
+
+
 }
